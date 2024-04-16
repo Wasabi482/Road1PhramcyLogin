e103d69ef271b9e5479023b89873b26fc5083a5c
Paul Benedict Santos <paulbenedicts482@gmail.com>
2024-04-16 17:46:57 +0800

    Folder Structure

---
+++ Road1PharmacyLogin-3/pages/Register.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Register Form</title>
+   <link rel="stylesheet" href="../css/main.css">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>   
+   <!-- SweetAlert2 CSS -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10"></link>
+   <!-- SweetAlert2 JS -->
+   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+   <!-- Optional Bootstrap JS -->
+   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+   
+</head>
+
+<body class="bg-dark background">
+   <div class="section ">
+      <div class="container ">
+         <div class="image">
+         <a href="Login.php" class="return"><img width="50" height="50" src="https://img.icons8.com/ios/50/FFFFFF/circled-left--v1.png" alt="circled-left--v1"/></a>
+            <div class="logo">
+               <img class="logo-image" src="../Images/IMG_5789__1_-removebg-preview.png" alt="logo">
+               <h1>Road 1 Pharmacy</h1>
+            </div>
+            <p>Your One Stop Healthcare Pharmacy</p>
+         </div>
+         <div class="form-section bg-dark" >
+            <form  method="post" action="../actions/register.php">
+               <?php
+                  if(isset($error) && !empty($error)){
+                     echo '<div class="alert alert-danger">';
+                     foreach($error as $message){
+                        echo "<p>$message</p>";
+                     }
+                     echo '</div>';
+                  }
+               ?>
+               <div class="input-form">
+                  <input type="text" class="input bg-dark" name="username" required placeholder="Username">
+                  <label class="hidden-label" for="username">Username</label>
+               </div>
+               <div class="input-form">
+                  <input type="password1" class="input bg-dark"  name="password1" required placeholder="Password">
+                  <label class="hidden-label" for="username">Password</label>
+               </div>
+               <div class="input-form">
+                  <input type="password2" class="input bg-dark"  name="password2" required placeholder="Confirm Password">
+                  <label class="hidden-label" for="username">Confirm Password</label>
+               </div>
+               <div class="input-form">
+                  <input type="email" class="input bg-dark"  name="email" required placeholder="Enter your email">
+                  <label class="hidden-label" for="username">Email</label>
+               </div>
+               <div class="input-form">
+                  <input type="file" id="myFile" name="filename">
+                  <label class="pic-label" for="filename">Profile Picture</label>
+               </div>
+               <div class="input-form">
+                  <select name="role" id="role" class="input bg-dark">
+                  <option value="">Please Choose a Role</option>
+                  <option value="Frontend">Frontend</option>
+                  <option value="RDU">RDU</option>
+                  <option value="Admin">Admin</option>
+                  </select>
+               </div>
+                  <button type="submit" name="submit" class="btn btn-primary button">Register</button>
+            </form>
+            <div class="other-sign-in">
+               <!--<p>Fast sign up with your favorite social profile</p>
+               <div class="icons">
+                  <a href="https://www.google.com"><i class="bi bi-google h2" style="color: orangered;"></i></a>
+                  <a href="https://www.facebook.com"><i class="bi bi-facebook h2" style="color: blue;"></i></a>
+                  <a href="https://www.twitter.com"><i class="bi bi-twitter h2" style="color: skyblue;"></i></a>
+               </div>-->
+                  <p>If you are alreay a member please <a href="Login.php">Sign In</a></p>
+            </div> 
+         </div>
+      </div>
+   </div>
+</body>
+</html>
---
+++ Road1PharmacyLogin-3/pages/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Road 1 Pharmacy</title>
+   <link rel="stylesheet" href="../css/main.css">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>   
+   <!-- SweetAlert2 CSS -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10"></link>
+   <!-- SweetAlert2 JS -->
+   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+   <!-- Optional Bootstrap JS -->
+   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body class="frontpage-background">
+    <div class="frontpage-container">
+        <nav class="nav-bar navbar navbar-expand-md navbar-dark" style="position:fixed">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">
+                    <img src="../Images/IMG_5789__1_-removebg-preview.png" class="logo-image-navbar h1" alt="logo">Road 1 Pharmacy
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav navbar-dropdown mr-auto">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Medicines</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Home</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">FAQ</a>
+                            </li>
+                        </ul>
+                        <!--<ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex ">
+                        <li class="nav-item navbar-button px-2">
+                            <a class="darkblue-button " href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item navbar-button px-2">
+                            <a class="darkblue-button " href="register.php">Register</a>
+                        </li>
+                    </ul>-->
+                </div>
+            </div>
+        </nav>
+        <div class="frontpage-section">
+            <div class="section-1">
+                <p class="quote">Your One Stop <br> Healthcare Pharmacy</p>
+                <div class="section-button">
+                    <a class="darkblue-button sec-button">About Us</a>
+                </div> 
+            </div>
+            <div class="section-2">
+                <img class="section-image" src="https://hcmpharmacy.pharmabest.ca/wp-content/uploads/2022/12/toronto-pharmacy.png" alt="">
+            </div>
+        </div>
+        <div class="trending-topics">
+        <h6 class="topics">Trending Topics</h6>
+            <div class="square-button">
+                <button class="box">VITAMINS<img class="img-topics" src="https://cdn-icons-png.flaticon.com/512/4887/4887988.png" alt=""></button>
+                <button class="box">FLU REMEDIES <img class="img-topics" src="https://static.vecteezy.com/system/resources/thumbnails/014/604/165/small/girl-sick-face-cartoon-cute-png.png" alt=""></button>
+                <button class="box">KIDS NUTRITION <img class="img-topics" src="https://img.pikbest.com/png-images/nutritious-and-delicious-soy-milk-png-elements_2495184.png!sw800" alt=""></button>
+                <button class="box">PRESCRIPTIONS <img class="img-topics" src="https://cdn-icons-png.flaticon.com/512/843/843180.png" alt=""></button>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
